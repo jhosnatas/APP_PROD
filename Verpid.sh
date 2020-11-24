49285177c438390bdc1e7c2bc9694ab0cfef3b1d
jhosnatas <42471567+jhosnatas@users.noreply.github.com>
2020-11-24 19:23:37 -0300

    Update Verpid.sh
    A saida sera uma lista de conexões ativas do sqlplus na maquina , ajudando na abertura de chamados .
    a sessions_longops ajuda a uma analise fácil se o processo esta travado mas apenas para sessões com durações maior de 6 segundos . 
    
    -----------------------------------------------------------------------------------------------------------------------------
     MODULO: @/arquivo_sql_em_execucao.tmp.sql.tmp
     SID: 3223
     SERIAL: 16707
     PROCESS: 55682
     USER: SYSADM
     STATUS: ACTIVE
     MACHINE: maquina111
     SQL_ID: dayga8dd464k
     LONGOPS: 37%
    ----------------------------------------------------------------------------------------------------------------------------
    (DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = hosttnsping)(PORT = 9999))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = BANCODBOORACLE)))
    ----------------------------------------------------------------------------------------------------------------------------

--- Verpid.sh
+++ Verpid.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
-### JONATAS SILVA
+### JONATAS SILVA 2020
 ### SCRIPT para pegar pid de processos conetactados ao oracle com sqlplus
 ### Necessario sysadm para acessos as views
 
 echo -e "|              `date`          |"
 DBUSER=$( cat ./dbuser )
+
+## gerar lista de processos sql , precisa ser adpdatado a sua necessidade
 TMPPID=$( ps -ef | grep "sqlplus" | grep @ | grep -v "replicador_status.sql" | awk {'print $2'} )
 for I in $(echo ${TMPPID[@]})
 do
-SID=$(sqlplus -s sysadm/${DBUSER}@$BSCSDB << END
+### || '|' ||
+### SELECT  sid, serial#, process, status, sql_id, machine , username
+
+SID=$(sqlplus -s sysadm/${DBUSER}@BANCODBOORACLE << END
         SET PAGESIZE 0 FEEDBACK off VERIFY off HEADING off WRAP off
-        SELECT sid, serial#, process, status, sql_id, machine
+        SELECT  sid || ' ' || serial# || ' ' || process || ' ' || status || ' ' || sql_id || ' ' || machine || ' ' || username
         FROM gv\$session  WHERE process in ('${I}');
 END
 )
-##tratando multiplos vetores no array
+##tratando multiplos vetores no array , vou validar a eficacio do  || ' ' || 
 for I in $(printf "%s\n" `echo ${SID[@]} |sed 's/ /\n/g'`) ; do ARRAY+=("${I}") ; done
 SID=("${ARRAY[@]}")
 
 ### SESSION_LONGOPS Views para acompanha a instrucao sql no momento
-PERC=$(sqlplus -s sysadm/${DBUSER}@$BSCSDB << END
+PERC=$(sqlplus -s sysadm/${DBUSER}@BANCODBOORACLE << END
         SET PAGESIZE 0 FEEDBACK off VERIFY off HEADING off WRAP off
         select round(sofar*100/totalwork,2) perc from GV\$SESSION_LONGOPS a where sid = '${SID[0]}' ;
 END
@@ -28,7 +33,7 @@ END
 echo -e "-----------------------------------------------------------------------------------------------------------------------------"
 mD=$( ps -ef | grep ${SID[2]} | grep -v grep | awk {'print $10, $11'} )
 echo -e " MODULO: ${mD}" ; echo -e " SID: ${SID[0]}" ; echo -e " SERIAL: ${SID[1]}" ; echo -e " PROCESS: ${SID[2]}"
- echo -e " USER: ${SID[7]}" ; echo -e " STATUS: ${SID[3]}" ; echo -e " MACHINE: ${SID[5]}" ;echo -e " SQL_ID: ${SID[4]}"
+ echo -e " USER: ${SID[6]}" ; echo -e " STATUS: ${SID[3]}" ; echo -e " MACHINE: ${SID[5]}" ;echo -e " SQL_ID: ${SID[4]}"
 ### Validando LONGOPS views
 for I in $(echo ${PERC[@]}) ; do I=$( echo $I |egrep -iv "ORA"  | cut -d '.' -f1 |sed 's/[^0-9]//g' )
 [[ "${I}" -lt "100" ]] && [[ "${I}" != "" ]] && echo -e " LONGOPS: ${I}%"
@@ -36,5 +41,5 @@ done
 unset SID ; unset ARRAY
 done
 echo -e "----------------------------------------------------------------------------------------------------------------------------"
-echo -e "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = scan-bscsix)(PORT = 1525))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = BSCSIXP1)))"
+echo -e "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = hosttnsping)(PORT = 9999))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = BANCODBOORACLE)))"
 echo -e "----------------------------------------------------------------------------------------------------------------------------"
