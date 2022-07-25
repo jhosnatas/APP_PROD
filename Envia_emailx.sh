cc18c090e146dacb3bbb3d9314f593ec6f837e78
jhosnatas <42471567+jhosnatas@users.noreply.github.com>
2022-07-25 16:32:47 -0300

    Update Envia_emailx.sh

--- Envia_emailx.sh
+++ Envia_emailx.sh
@@ -1,21 +1,36 @@
-#!/bin/sh
-# Enviando por email utilizando mailx
-# utizando array ao inves de criar arquivos 
-# JONATAS SILVA
-MASK="*.csv"
-PARA="jonatassilvas@jonatassilvas.com.br jonatassilvas@jonatassilvas.com.br"
-TITLE="[MODULO NOME] RELATORIO NOME [$(date +%d/%m/%Y)]"
+#!/bin/bash
 
-if [ "$(ls $MASK 2> /dev/null | wc -l)" == "0" ] ; then
-        EMAIL="\n\nNao existem arquivos RELATORIO NOME  para enviar\n\nAtt. MEU TIME DE RELATORIOS"
-        echo -e ${EMAIL[@]} | mailx -r EXTRATOR_ETL -s "${TITLE}"  $PARA
+#------------------------------------------------------
+### JONATAS SILVA 2020
+### Enviando anexos automaticamente com mailx
+### http://jhosnatas.github.io/ 
+### 
+#------------------------------------------------------
+
+## Variaveis
+MASK="*.csv" # Mascara dos arquivos
+PARA="jonatassilvas@jonatassilvas.com.br jonatassilvas@jonatassilvas.com.br" # Lista email
+TITLE="[MODULO NOME] RELATORIO NOME [$(date +%d/%m/%Y)]" # Titulo do email
+
+## Enviando o email
+if [ "$(ls $MASK 2> /dev/null | wc -l)" == "0" ] ; then ## Valida a existencia dos arquivos
+        ## email caso NAO exista arquivos
+        EMAIL="\n\nNao existem arquivos RELATORIO NOME  para enviar\n\nAtt. MEU TIME DE RELATORIOS" ## corpo do email
+        echo -e ${EMAIL[@]} | mailx -r EXTRATOR_ETL -s "${TITLE}"  $PARA ## Enviando o email
+                [[ $? != 0 ]] && echo "ERRO ao enviar email " && exit 1 ## Valida envio do email
         else
-                EMAIL="\n \n  Segue lista de arquivos RELATORIO NOME ANEXOS \n \n" ##cabecalho
+                ## email caso exista arquivos
+                EMAIL="\n \n  Segue lista de arquivos RELATORIO NOME ANEXOS \n \n" ## Cabecalho
                 EMAIL+=$(ls ${MASK} | awk '{print $1 "\n"}' ) ## corpo do email
-                EMAIL+="\n \n Att. MEU TIME DE RELATORIOS" ## rodade
-                ANEXOS+=$(ls ${MASK} | awk '{print "-a " $1}' ) ### lista de anexos
+                EMAIL+="\n \n Att. MEU TIME DE RELATORIOS" ## Rodade
+                ANEXOS+=$(ls ${MASK} | awk '{print "-a " $1}' ) ## Lista de anexos
                 echo -e ${EMAIL[@]} |  mailx -r EXTRATOR_ETL -s "${TITLE}" ${ANEXOS} $PARA #envia email
-                 for x in $(ls $MASK ) ; do mv $x /enviados/ ; done # move arquivos para o backupi
+                        [[ $? != 0 ]] &&  echo "ERRO ao enviar email " && exit 1 ## Valida envio do email
+                for x in $(ls $MASK ) ; do mv $x /enviados/ ; done ## Move arquivos para o backup
 fi
+
+
+exit 0
+
 # Final do Shell
 
